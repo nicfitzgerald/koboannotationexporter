9cd2bde6f9cc4d4445ec55bae7b9aa47ace43b80
Nic FitzGerald <nicholas.a.fitzgerald@gmail.com>
2025-06-18 02:49:27 -0500

    [Feat] Correct DB picking implementation

--- frontend/src/routes/database/+page.svelte
+++ frontend/src/routes/database/+page.svelte
@@ -3,22 +3,26 @@
   let items = $state('')
   let path = $state('')
 
-  // function getDBPath() {
-  //   GetDBPath().then((result) => {
-  //     path = result
-  //   })
-  // }
-  function getInfoFromDB(path) {
-    items = GetInfoFromDB(path)
+  function getDBPath() {
+    GetDBPath().then((result) => {
+      path = result
+    })
+  }
+  function getInfoFromDB() {
+    GetInfoFromDB(path).then((result) => {
+      items = result
+    })
     return items
   }
 </script>
 
 <h1>Database Info!</h1>
-<!-- <button onclick={getDBPath}>Select DB</button><br />
-<span>The current path is: {path}</span>
-<hr /> -->
-<button onclick={getInfoFromDB('/Users/nicfitzgerald/Dev/github.com/nicfitzgerald/koboexport/reader.sqlite')}>Get Info</button>
+<button onclick={getDBPath}>Select DB</button><br />
+<span>The current path is: {path}</span><br />
+<br />
+<hr />
+<br />
+<button onclick={getInfoFromDB}>Get Info</button>
 <p>This is what the DB shows:</p>
 {#each items.annotations as author}
   <br />
@@ -26,7 +30,7 @@
   {#each author.books as book}
     <h2>{book.title}</h2>
     {#each book.excerpts as excerpt}
-      <span>{excerpt.text}, created at {excerpt.createdAt}</span><br />
+      <span>{excerpt.text} (created at <strong>{excerpt.createdAt}</strong>)</span><br />
     {/each}
   {/each}
 {/each}
